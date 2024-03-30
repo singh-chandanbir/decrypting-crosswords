cafc75e2251aa96104a615ee10f6facb7bf98488
0Bharat9 <bharatexhash09@gmail.com>
2024-03-30 16:32:19 +0530

    updated-solver

--- crosswords/crosswordsolver/DPR/dpr/models/reader.py
+++ crosswords/crosswordsolver/DPR/dpr/models/reader.py
@@ -19,8 +19,8 @@ import torch.nn as nn
 from torch import Tensor as T
 from torch.nn import CrossEntropyLoss
 
-from dpr.data.reader_data import ReaderSample, ReaderPassage
-from dpr.utils.model_utils import init_weights
+from crosswords.crosswordsolver.DPR.dpr.data.reader_data import ReaderSample, ReaderPassage
+from crosswords.crosswordsolver.DPR.dpr.utils.model_utils import init_weights
 
 logger = logging.getLogger()
 
